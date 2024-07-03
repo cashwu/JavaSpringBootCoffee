6c0f92fd64c9a79c8bb4b6a4c22ba1d346675aa2
Cash <cashwugeek@icloud.com>
2024-07-03 14:09:49 +0800

    init

---
+++ src/main/java/com/cashwu/javaspringbootcoffee/JavaSpringBootCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaspringbootcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashwu/javaspringbootcoffee/JavaSpringBootCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaspringbootcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
